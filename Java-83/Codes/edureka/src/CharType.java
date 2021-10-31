5831d0c1477da8a5b19fe605bcecffae9ca1796b
Sunil Joseph <sunil.edureka@gmail.com>
2021-10-31 10:16:28 +0530

    java-83

---
+++ Java-83/Codes/edureka/src/CharType.java
@@ -0,0 +1,28 @@
+
+public class CharType {
+
+	public static void main(String[] args) {
+		char c = 'A';
+		System.out.println(c);
+		
+		c = 100; //implicit typecasting
+		System.out.println(c);
+		
+		c = '\u0041';
+		System.out.println(c);
+		
+		c = '\u0905';
+		System.out.println(c);
+		
+		c = '\u0c05';
+		System.out.println(c);
+		
+		c = '\u4eca';
+		System.out.println(c);
+		
+		//System.out.println(Integer.toHexString(65));
+		
+		int n = 'd';
+		System.out.println(n);
+	}
+}
---
+++ Java-83/Codes/edureka/src/ConditionalOperator.java
@@ -0,0 +1,11 @@
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		int x = 10, y=20;
+		
+		int lar = (x>y)?x:y;
+		
+		System.out.println("Largest = " + lar);
+	}
+}
